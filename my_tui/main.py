16dbe98b16a7752e4354652372e3586417baaadf
Андрей Тарасенко <tarasenko@algont.ru>
2024-10-25 16:47:28 +0300

    add: Добавлен аргумент для пути репозитория

--- my_tui/main.py
+++ my_tui/main.py
@@ -12,6 +12,10 @@ import questionary
 from rich.console import Console
 from rich.panel import Panel
 
+import argparse
+from pathlib import Path
+
+
 console = Console()
 
 
@@ -123,44 +127,144 @@ def convert_input_date_to_commit_date(date_string: str) -> str:
     return dt.strftime("%a %b %d %H:%M:%S %Y %z")
 
 
+# def get_git_log(repo_path: str, timeout: int = 10) -> List[str]:
+#     """
+#     Retrieves the Git log output for a given repository.
+#     Args:
+#         repo_path (str): Path to the Git repository.
+#         timeout (int, optional): Timeout in seconds. Defaults to 10.
+
+#     Returns:
+#         List[str]: Output lines from the Git log command.
+#     """
+#     cmd = "git log --pretty"
+#     try:
+#         process = subprocess.Popen(
+#             cmd,
+#             cwd=repo_path,
+#             shell=True,
+#             stdout=subprocess.PIPE,
+#             stderr=subprocess.PIPE,
+#             preexec_fn=os.setsid,
+#         )
+#         stdout, _ = process.communicate(timeout=timeout)
+
+#         if process.returncode != 0:
+#             raise subprocess.CalledProcessError(process.returncode, cmd, stdout)
+#     except subprocess.CalledProcessError as e:
+#         sys.stderr.write(e.output)
+#     except subprocess.TimeoutExpired:
+#         os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+#         sys.stderr.write("Killed by timeout")
+#     else:
+#         return str(stdout, "utf-8").split("\n")
+
+
+# GIT_SET_DATE_TEMPLATE = 'git filter-branch -f --env-filter \'if [ $GIT_COMMIT = {} ]; then export GIT_COMMITTER_DATE="{}"; export GIT_AUTHOR_DATE="{}"; fi\''
+
+
+# def set_git_date(
+#     repo_path: str, commit: Commit, new_date: str, timeout: int = 600
+# ) -> None:
+#     """
+#     Sets a new date for a Git commit.
+
+#     Args:
+#         repo_path (str): Path to the Git repository.
+#         commit (Commit): Commit object.
+#         new_date (str): New date in '2024.10.8 11:59:23 +0300' format.
+#         timeout (int, optional): Timeout in seconds. Defaults to 600.
+#     """
+#     cmd = GIT_SET_DATE_TEMPLATE.format(commit.hash, new_date, new_date)
+#     try:
+#         process = subprocess.Popen(
+#             cmd,
+#             cwd=repo_path,
+#             shell=True,
+#             stdout=subprocess.PIPE,
+#             stderr=subprocess.PIPE,
+#             preexec_fn=os.setsid,
+#         )
+#         stdout, _ = process.communicate(timeout=timeout)
+
+#         if process.returncode != 0:
+#             raise subprocess.CalledProcessError(process.returncode, cmd, stdout)
+#     except subprocess.CalledProcessError as e:
+#         sys.stderr.write(e.output)
+#     except subprocess.TimeoutExpired:
+#         os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+#         sys.stderr.write("Killed by timeout")
+
+
+def execute_system_command(
+    cmd: str, 
+    cwd: str, 
+    timeout: int = 10
+) -> tuple:
+    """
+    Выполняет системную команду с обработкой ошибок и таймаутом.
+
+    Args:
+        cmd (str): Системная команда для выполнения.
+        cwd (str): Рабочая директория для выполнения команды.
+        timeout (int, optional): Таймаут в секундах. Defaults to 10.
+
+    Returns:
+        tuple: Кортеж, содержащий вывод команды (stdout) и индикатор ошибки (bool).
+               Если команда выполнилась успешно, возвращает (stdout, False), 
+               в случае ошибки или таймаута - (error_message, True).
+    """
+    try:
+        process = subprocess.Popen(
+            cmd,
+            cwd=cwd,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            preexec_fn=os.setsid
+        )
+        stdout, _ = process.communicate(timeout=timeout)
+        
+        if process.returncode != 0:
+            raise subprocess.CalledProcessError(process.returncode, cmd, stdout)
+        
+        return str(stdout, "utf-8"), False
+    
+    except subprocess.CalledProcessError as e:
+        sys.stderr.write(f"Command '{cmd}' failed with error:\n{e.output}")
+        return str(e.output), True
+    
+    except subprocess.TimeoutExpired:
+        os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+        error_msg = f"Command '{cmd}' killed by timeout ({timeout}s)"
+        sys.stderr.write(error_msg + "\n")
+        return error_msg, True
+
+
+GIT_LOG_CMD = "git log --pretty"
+GIT_SET_DATE_TEMPLATE = 'git filter-branch -f --env-filter \'if [ $GIT_COMMIT = {} ]; then export GIT_COMMITTER_DATE="{}"; export GIT_AUTHOR_DATE="{}"; fi\''
+
+
 def get_git_log(repo_path: str, timeout: int = 10) -> List[str]:
     """
     Retrieves the Git log output for a given repository.
+
     Args:
         repo_path (str): Path to the Git repository.
         timeout (int, optional): Timeout in seconds. Defaults to 10.
 
     Returns:
-        List[str]: Output lines from the Git log command.
+        List[str]: Output lines from the Git log command or an empty list on error.
     """
-    cmd = "git log --pretty"
-    try:
-        process = subprocess.Popen(
-            cmd,
-            cwd=repo_path,
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            preexec_fn=os.setsid,
-        )
-        stdout, _ = process.communicate(timeout=timeout)
-
-        if process.returncode != 0:
-            raise subprocess.CalledProcessError(process.returncode, cmd, stdout)
-    except subprocess.CalledProcessError as e:
-        sys.stderr.write(e.output)
-    except subprocess.TimeoutExpired:
-        os.killpg(os.getpgid(process.pid), signal.SIGTERM)
-        sys.stderr.write("Killed by timeout")
-    else:
-        return str(stdout, "utf-8").split("\n")
-
-
-GIT_SET_DATE_TEMPLATE = 'git filter-branch -f --env-filter \'if [ $GIT_COMMIT = {} ]; then export GIT_COMMITTER_DATE="{}"; export GIT_AUTHOR_DATE="{}"; fi\''
+    stdout, error_occurred = execute_system_command(GIT_LOG_CMD, repo_path, timeout)
+    return stdout.split("\n") if not error_occurred else []
 
 
 def set_git_date(
-    repo_path: str, commit: Commit, new_date: str, timeout: int = 600
+    repo_path: str, 
+    commit: 'Commit',  # Принято использовать строковые литералы для forward reference
+    new_date: str, 
+    timeout: int = 600
 ) -> None:
     """
     Sets a new date for a Git commit.
@@ -172,31 +276,26 @@ def set_git_date(
         timeout (int, optional): Timeout in seconds. Defaults to 600.
     """
     cmd = GIT_SET_DATE_TEMPLATE.format(commit.hash, new_date, new_date)
-    try:
-        process = subprocess.Popen(
-            cmd,
-            cwd=repo_path,
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            preexec_fn=os.setsid,
-        )
-        stdout, _ = process.communicate(timeout=timeout)
-
-        if process.returncode != 0:
-            raise subprocess.CalledProcessError(process.returncode, cmd, stdout)
-    except subprocess.CalledProcessError as e:
-        sys.stderr.write(e.output)
-    except subprocess.TimeoutExpired:
-        os.killpg(os.getpgid(process.pid), signal.SIGTERM)
-        sys.stderr.write("Killed by timeout")
+    execute_system_command(cmd, repo_path, timeout)
 
 
 def main() -> None:
     """
     Main application loop.
     """
-    repo_path = "/home/andrew/TEST_GIT"  # TODO: Add argument for repository path
+    # repo_path = "/home/andrew/TEST_GIT"  # TODO: Add argument for repository path
+    parser = argparse.ArgumentParser(description='Git Commit Date Changer')
+    parser.add_argument('repo_path', type=str, help='Path to the Git repository')
+    args = parser.parse_args()
+
+    # Приводим путь к абсолютному
+    repo_path = Path(args.repo_path).resolve()
+
+    # Проверяем, что путь указывает на существующую директорию (не обязательно, но рекомендуется для повышения robustness)
+    if not repo_path.is_dir():
+        console.print(f"[red]Ошибка: '{repo_path}' не является существующей директорией.[/red]")
+        return
+    console.print(Panel(str(repo_path), highlight=True, title="Выбранный репозиторий"))
     while True:
         output_lines = get_git_log(repo_path)
         commits = parse_git_log(output_lines)
